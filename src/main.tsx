b39cc48478922cc441cfe8d4fa04fe028cc759e4
redfisharthur-beep <redfisharthur@gmail.com>
2026-09-01 07:57:30 +0800

    Load battle result capsule styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import './jobPortraits.css'
+import './battle-results.css'
 import { installJobPortraits } from './jobPortraits'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
