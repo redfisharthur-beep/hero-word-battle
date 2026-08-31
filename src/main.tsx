53f1aa75f8f47fe372ac05ee5bbafd3089d5fbbc
redfisharthur-beep <redfisharthur@gmail.com>
2026-08-31 21:20:52 +0800

    Enable job portraits in live UI

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import './jobPortraits.css'
+import { installJobPortraits } from './jobPortraits'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+installJobPortraits()
